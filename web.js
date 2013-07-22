7cc2de34d21856ec64af45e01bf807f9a81ad1e7
Jon Hjerting <jon@hjerting.com>
2013-07-22 12:00:16 +0000

    update bootstrap

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
 app.use(express.logger());
 
+var html = fs.readFileSync('index.html', 'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
